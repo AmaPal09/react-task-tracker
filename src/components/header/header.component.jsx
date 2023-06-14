dcad202b2638afe03e16d8e233d15c2435ef5203
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-14 08:22:48 -0700

    CHNG: Remove add button from about page
    Use new hook useLocation to get location where add button to be displayed

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,17 +2,19 @@
 import Button from '../button/button.component';
 
 import PropTypes from 'prop-types'; 
+import { useLocation } from 'react-router-dom'; 
 
 
 const Header = ({ title, onAdd, showAdd  }) => {
+    const location = useLocation(); 
 
     return(
         <header className='header'>
             <h1>{title}</h1>
-            <Button 
+            { location.pathname==='/' && <Button 
             color={showAdd ? 'red' : 'green'} 
             text={showAdd ? 'Close' : 'Add'} 
-            onClick={onAdd} /> 
+            onClick={onAdd} /> }
         </header>
     )
 };
