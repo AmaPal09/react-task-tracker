39855130feb052280e0c3dab21b1292b5c48cf77
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-15 07:11:45 -0700

    DOC: Remove commented code

--- src/routes/home/home.route.component.jsx
+++ src/routes/home/home.route.component.jsx
@@ -16,67 +16,6 @@ import About from '../../components/about/about.component';
 function Home() {
     const { showAddTask } = useContext(OnAddContext);
     const {taskList, setTaskList, deleteTask, toggleReminder, addTask } = useContext(TaskListContext);   
-    // const [taskList, setTaskList] = useState([]);
-    
-    // useEffect(()=> {
-    //     const getTasks = async () => {
-    //         const tasksFromServer = await fetchTasks()
-    //         console.log(tasksFromServer); 
-    //         setTaskList(tasksFromServer)
-    //     } 
-        
-    //     getTasks();         
-    // }, []);
-
-    //Fetch tasks from API 
-    // async function fetchTasks() {
-    //     const res = await fetch('http://localhost:5100/tasks');
-    //     const data = await res.json();
-    //     return data;
-    // }
-
-    //Fetch single task from API 
-    // const fetchTask = async (id) =>  {
-    //     const res = await fetch(`http://localhost:5100/tasks/${id}`);
-    //     const data = await res.json();
-    //     return data;
-    // }
-
-    //toggle task reminders 
-    // const toggleReminder = async (id) => {
-    //     const taskToToggle = await fetchTask(id); 
-    //     const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}; 
-
-    //     const res = await fetch(`http://localhost:5100/tasks/${id}`, {
-    //         method: 'PUT',
-    //         headers: {
-    //             'Content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(updatedTask)
-    //     })
-        
-    //     const data = await res.json(); 
-
-    //     // console.log('toggle', id); 
-    //     setTaskList(taskList.map((task) => task.id === id ? 
-    //         {...task, reminder: data.reminder} : 
-    //         task
-    //     )); 
-    // }
-
-    //Add tasks 
-    // const addTask = async (task) => {
-    //     const res = await fetch('http://localhost:5100/tasks', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-type': 'application/json'
-    //         }, 
-    //         body: JSON.stringify(task)
-    //     })
-    //     const data = await res.json()
-
-    //     setTaskList([...taskList, data]); 
-    // } 
 
   return (
     <div className="task-tracker-container">
