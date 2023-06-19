7edab5bade064de7e0c7a2c6494677e21d05bda1
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-19 13:25:25 -0700

    DOC: Remove commented code

--- src/components/add-task-form/add-task-form.component.jsx
+++ src/components/add-task-form/add-task-form.component.jsx
@@ -34,88 +34,37 @@ const AddTaskForm = () => {
         setReminder(false); 
     }
 
-
-    // return (
-    //     <form className="add-form" onSubmit={submitForm}>
-    //         <div className="form-control" >
-    //             <label>Task</label>
-    //             <input 
-    //                 type="text" 
-    //                 value={text}
-    //                 placeholder="Add your task" 
-    //                 required
-    //                 onChange={(e) => setText(e.target.value)} />
-    //         </div>
-    //         <div className="form-control">
-    //             <label>Day & Time</label>
-    //             <input 
-    //                 type="text" 
-    //                 value={day}
-    //                 placeholder="Add day & time" 
-    //                 onChange={(e) => setDay(e.target.value)} />
-    //         </div>
-    //         <div className="form-control form-control-check">
-    //             <label>Set Reminder</label>
-    //             <input 
-    //                 type="checkbox"
-    //                 value={reminder} 
-    //                 checked={reminder}
-    //                 onChange={(e)=> setReminder(e.currentTarget.checked)} />
-    //         </div>
-    //         <input 
-    //             type="submit" 
-    //             value="Save Task" 
-    //             // className="btn btn-block" 
-    //             className="submit-btn"/> 
-    //     </form>
-    // )
     return (
-        // <form className="add-form" onSubmit={submitForm}>
         <AddForm onSubmit={submitForm}> 
-            {/* <div className="form-control" > */}
             <FormControl > 
-                {/* <label>Task</label> */}
                 <FormControlLabel> Task </FormControlLabel>
-                {/* <input  */}
                 <FormControlInput 
                     type="text" 
                     value={text}
                     placeholder="Add your task" 
                     required
                     onChange={(e) => setText(e.target.value)} />
-            {/* </div> */}
             </FormControl>
-            {/* <div className="form-control"> */}
             <FormControl> 
-                {/* <label>Day & Time</label> */}
                 <FormControlLabel> Day & Time </FormControlLabel>
-                {/* <input  */}
                 <FormControlInput 
                     type="text" 
                     value={day}
                     placeholder="Add day & time" 
                     onChange={(e) => setDay(e.target.value)} />
-            {/* </div> */}
             </FormControl>
-            {/* <div className="form-control form-control-check"> */}
             <FormControlCheck>
-                {/* <label>Set Reminder</label> */}
                 <FormControlCheckLabel> Set Reminder </FormControlCheckLabel>
-                {/* <input  */}
                 <FormControlCheckInput
                     type="checkbox"
                     value={reminder} 
                     checked={reminder}
                     onChange={(e)=> setReminder(e.currentTarget.checked)} />
-            {/* </div> */}
             </FormControlCheck>
-            {/* <input  */}
             <SubmitInput
                 type="submit" 
                 value="Save Task" 
-                // className="btn btn-block" 
                 className="submit-btn"/> 
-        {/* // </form> */}
         </ AddForm> 
     )
 
