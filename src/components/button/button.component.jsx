ef8ba1c02a60ca3e3d96b2cca9c148b3b112585b
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-19 13:36:20 -0700

    DOC: Remove commented code

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,18 +1,15 @@
 //button.component.jsx
 import PropTypes from 'prop-types'; 
 
-// import './button.component.styles.css'; 
 import StyledButton from './button.component.styled'; 
 
 const Button = ({ color, text, onClick}) => {
     return(
-        // <button 
         <StyledButton
             style={{ backgroundColor: color}} 
             className="btn"
             onClick={onClick}>
                 {text}  
-        {/* </button> */}
         </StyledButton>
     )
 }; 
