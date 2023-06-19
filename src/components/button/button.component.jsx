47648795ba2174259914aa3ef5e22b61e6374b04
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-19 13:34:46 -0700

    CHNG: Use custom styled component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,17 +1,19 @@
 //button.component.jsx
 import PropTypes from 'prop-types'; 
 
-import './button.component.styles.css'; 
-
+// import './button.component.styles.css'; 
+import StyledButton from './button.component.styled'; 
 
 const Button = ({ color, text, onClick}) => {
     return(
-        <button 
+        // <button 
+        <StyledButton
             style={{ backgroundColor: color}} 
             className="btn"
             onClick={onClick}>
                 {text}  
-        </button>
+        {/* </button> */}
+        </StyledButton>
     )
 }; 
 
