10771bab1eac22277d275f26b9ab5cbed849c347
Amarja Palekar <amarjapalekar09@gmail.com>
2023-06-19 15:45:50 -0700

    DOC: Remove commented code

--- src/components/task/task.component.jsx
+++ src/components/task/task.component.jsx
@@ -5,25 +5,21 @@ import { useContext } from 'react';
 import { FaTimes } from 'react-icons/fa'; 
 
 import { TaskListContext } from '../../contexts/taskList.context';
-// import './task.component.styles.css'; 
+
 import { SubHeader3, TaskContainer } from './task.styled.component';
 
 const Task = ({ task }) => {
     const {deleteTask, toggleReminder} = useContext(TaskListContext);
     
     return (
-        // <div 
         <TaskContainer
-            // className={`task ${task.reminder ? 'reminder': ''}`} 
             border={task.reminder}
             onDoubleClick={()=> toggleReminder(task.id)}>
-                {/* <h3> */}
                 <SubHeader3> 
                     {task.text}{'   '} 
                     <FaTimes 
                         style={{color:'red', cursor:'pointer'}} 
                         onClick={() => deleteTask(task.id)}/> 
-                {/* </h3> */}
                 </SubHeader3>
                 <p>{task.day}</p>            
         </TaskContainer>
